656106ff6eafd185acce7c20f0fa78307047b5ee
Dmitry Tyurnikov <DimsFromDergachy@outlook.com>
2024-12-01 01:50:57 +0400

    Solve 8-th part B

--- src/Solvers/2015/Day08.cs
+++ src/Solvers/2015/Day08.cs
@@ -5,7 +5,16 @@ class SolverA : Solver
 {
     internal override object Solve(string input) =>
         input.Lines()
-             .Select(line => 2 + line.Count() - line.Unescape().Count())
+             .Select(line => line.Count() - line.Unescape().Count())
+             .Sum();
+}
+
+[Solver(2015, 08, Part.B)]
+class SolverB : Solver
+{
+    internal override object Solve(string input) =>
+        input.Lines()
+             .Select(line => line.Escape().Count() - line.Count())
              .Sum();
 }
 
@@ -15,7 +24,7 @@ internal static class StringExtensions
     internal static IEnumerable<char> Unescape(this string source)
     {
         var context = string.Empty;
-        foreach (char ch in source)
+        foreach (char ch in source.Skip(1).SkipLast(1))
         {
             switch (ch, context)
             {
@@ -45,7 +54,7 @@ internal static class StringExtensions
                     if (context.Count() == 3)
                     {
                         context += ch;
-                        yield return '#'; // TODO: Doesn't matter context
+                        yield return '\''; // TODO: Doesn't matter context
                         context = string.Empty;
                     }
                     else
@@ -55,23 +64,64 @@ internal static class StringExtensions
             }
         }
     }
+
+    internal static IEnumerable<char> Escape(this string source)
+    {
+        yield return '\"';
+        foreach (var ch in source)
+        {
+            switch (ch)
+            {
+                case '\\':
+                {
+                    yield return '\\';
+                    yield return '\\';
+                    break;
+                }
+                case '\"':
+                {
+                    yield return '\\';
+                    yield return '\"';
+                    break;
+                }
+                default:
+                {
+                    yield return ch;
+                    break;
+                }
+            }
+        }
+        yield return '\"';
+    }
 }
 
 public class Test
 {
     [Theory]
-    [InlineData(@"""""", 2)]            //  ""          ->
-    [InlineData(@"""a""", 2)]           //  "a"         ->  a
-    [InlineData(@"""abc""", 2)]         //  "abc"       ->  abc
-    [InlineData(@"""aaa\""aaa""", 3)]   //  "aaa\"aaa"  ->  aaa"aaa
-    [InlineData(@"""\x27""", 5)]        //  "\x27"      ->  '
-    [InlineData(@"""\\""", 3)]          //  "\\"        ->  \
-    [InlineData(@"""\\\\""", 4)]        //  "\\\\"      ->  \\
-    [InlineData(@"""\\x""", 3)]         //  "\\x"       ->  \x
-    public void Example(string input, int expected)
+    [InlineData(@"""""", "")]                   //  ""          ->
+    [InlineData(@"""a""", "a")]                 //  "a"         ->  a
+    [InlineData(@"""abc""", "abc")]             //  "abc"       ->  abc
+    [InlineData(@"""aaa\""aaa""", "aaa\"aaa")]  //  "aaa\"aaa"  ->  aaa"aaa
+    [InlineData(@"""\x27""", "'")]              //  "\x27"      ->  '
+    [InlineData(@"""\\""", "\\")]               //  "\\"        ->  \
+    [InlineData(@"""\\\\""", "\\\\")]           //  "\\\\"      ->  \\
+    [InlineData(@"""\\x""", "\\x")]             //  "\\x"       ->  \x
+    public void Unescape(string input, string expected)
     {
         Assert.StartsWith("\"", input);
         Assert.EndsWith("\"", input);
-        Assert.Equal(expected, new SolverA().Solve(input));
+        Assert.Equal(expected, new string(input.Unescape().ToArray()));
+    }
+
+    [Theory]
+    [InlineData(@"""""", @"""\""\""""")]                        //  ""          ->  "\"\""
+    [InlineData(@"""abc""", @"""\""abc\""""")]                  //  "abc"       ->  "\"abc\""
+    [InlineData(@"""aaa\""aaa""", @"""\""aaa\\\""aaa\""""")]    //  "aaa\"aaa"  ->  "\"aaa\\\"aaa\""
+    [InlineData(@"""\x27""", @"""\""\\x27\""""")]               //  "\x27"      ->  "\"\\x27\""
+    public void Escape(string input, string expected)
+    {
+        Assert.StartsWith("\"", input);
+        Assert.EndsWith("\"", input);
+        Assert.Equal(expected, new string(input.Escape().ToArray()));
     }
 }
