a3f5154e716dbf3db6e32b2cfd807a64ab5b38bb
Dmitry Tyurnikov <DimsFromDergachy@outlook.com>
2024-12-02 01:25:25 +0400

    Fix testing

--- src/Extensions/EnumerableExtensions.cs
+++ src/Extensions/EnumerableExtensions.cs
@@ -16,14 +16,46 @@ static class EnumerableExtensions
     internal static (IEnumerable<T1>, IEnumerable<T2>) Unzip<T1, T2>(
         this IEnumerable<(T1, T2)> source) =>
             (source.Select(item => item.Item1), source.Select(item => item.Item2));
+
+    internal static IEnumerable<IEnumerable<TItem>> Permutations<TItem>(
+        this IEnumerable<TItem> source)
+        {
+            if (source.Any())
+            {
+                foreach (var item in source)
+                {
+                    var lists = source.Except(new List<TItem> {item})
+                                      .Permutations();
+                    foreach (var list in lists)
+                    {
+                        yield return list.Prepend(item);
+                    }
+                }
+            }
+            else
+            {
+                yield return Enumerable.Empty<TItem>();
+            }
+        }
 }
 
 public class EnumerableExtensionsTest
 {
     [Fact]
-    public void ScanTest()
+    public void Scan()
     {
         var array = new int[] { 1, 2, 3, 4, 5 };
         Assert.Equal([0, 1, 3, 6, 10, 15], array.Scan(0, (x, y) => x + y));
+    }
+
+    [Fact]
+    public void Permutations()
+    {
+        var array = new int[] { 1, 2, 3 };
+        Assert.Equal(1 * 2 * 3, array.Permutations().Count());
+        Assert.All(array.Permutations(), p =>
+            {
+                Assert.Equal([1, 2, 3], p.Order());
+            });
     }
 }
---
+++ src/Solvers/2015/Day09.cs
@@ -0,0 +1,9 @@
+namespace Year2015.Day09;
+
+class SolverA : Solver
+{
+    internal override object Solve(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
