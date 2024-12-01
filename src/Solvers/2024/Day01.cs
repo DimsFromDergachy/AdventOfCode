0b290f906b067849e73b8eb8e04bf87d70cc7f2e
Dmitry Tyurnikov <DimsFromDergachy@outlook.com>
2024-12-01 20:54:57 +0400

    Solve Part B

--- src/Solvers/2024/Day01.cs
+++ src/Solvers/2024/Day01.cs
@@ -1,6 +1,7 @@
 namespace Year2024.Day01;
 
 [Solver(2024, 01, Part.A)]
+[Solver(2024, 01, Part.B)]
 class Historian : Solver
 {
     internal override object Solve(string input)
@@ -12,8 +13,25 @@ class Historian : Solver
              .Select(nums => (nums[0], nums[1]))
              .Unzip();
 
-        return list1.Order()
-                    .Zip(list2.Order(), (a, b) => Math.Abs(a - b))
+        #pragma warning disable CS8524
+        return Part switch
+        {
+            Part.A => SolveA(list1, list2),
+            Part.B => SolveB(list1, list2),
+        };
+    }
+
+    int SolveA(IEnumerable<int> list1, IEnumerable<int> list2) =>
+        list1.Order()
+             .Zip(list2.Order(), (a, b) => Math.Abs(a - b))
+             .Sum();
+
+    int SolveB(IEnumerable<int> list1, IEnumerable<int> list2)
+    {
+        var group = list2.GroupBy(x => x)
+                         .ToDictionary(group => group.Key);
+
+        return list1.Select(x => group.ContainsKey(x) ? x * group[x].Count() : 0)
                     .Sum();
     }
 }
@@ -31,6 +49,10 @@ public class Test
 3   9
 3   3";
 
-        Assert.Equal(11, new Historian().Solve(input));
+        var solver = new Historian();
+        Assert.Equal(11, solver.Solve(input));
+
+        solver.Part = Part.B;
+        Assert.Equal(31, solver.Solve(input));
     }
 }
