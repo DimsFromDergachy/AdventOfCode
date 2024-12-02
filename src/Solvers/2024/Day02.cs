6df2a6f7ba005476e815706a97249b5a2b87fad3
Dmitry Tyurnikov <DimsFromDergachy@outlook.com>
2024-12-02 11:17:23 +0400

    Solve the day (Part B)

--- src/Solvers/2024/Day02.cs
+++ src/Solvers/2024/Day02.cs
@@ -1,12 +1,13 @@
 namespace Year2024.Day02;
 
 [Solver(2024, 02, Part.A)]
+[Solver(2024, 02, Part.B)]
 class RedNosedChecker : Solver
 {
     internal override object Solve(string input) =>
         input.Lines()
              .Select(line => line.Words().Select(int.Parse))
-             .Count(IsSave);
+             .Count(Part == Part.A ? IsSave : IsSaver);
 
     bool IsSave(IEnumerable<int> report) =>
         report.Zip(report.Skip(1), (a, b) => a - b)
@@ -14,4 +15,9 @@ class RedNosedChecker : Solver
             ||
         report.Zip(report.Skip(1), (a, b) => b - a)
               .All(x => 1 <= x && x <= 3);
+
+    bool IsSaver(IEnumerable<int> report) =>
+        report.Extract()
+              .Select(pair => pair.Item2)
+              .Any(IsSave);
 }
