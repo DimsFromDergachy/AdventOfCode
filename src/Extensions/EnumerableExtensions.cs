8f9bf6b913ec64d693313b6f7280537c8f9f6773
Dmitry Tyurnikov <DimsFromDergachy@outlook.com>
2024-12-02 11:13:24 +0400

    Extract

--- src/Extensions/EnumerableExtensions.cs
+++ src/Extensions/EnumerableExtensions.cs
@@ -17,26 +17,48 @@ static class EnumerableExtensions
         this IEnumerable<(T1, T2)> source) =>
             (source.Select(item => item.Item1), source.Select(item => item.Item2));
 
+    internal static IEnumerable<(TItem, IEnumerable<TItem>)> Extract<TItem>(
+        this IEnumerable<TItem> source)
+    {
+        if (!source.Any())
+            throw new ArgumentException("Empty collection");
+
+        // source === head ++ [elem] ++ tail
+        var head = Enumerable.Empty<TItem>();
+        var elem = source.First();
+        var tail = source.Skip(1);
+
+        yield return (elem, tail);
+        while (tail.Any())
+        {
+            head = head.Append(elem);
+            elem = tail.First();
+            tail = tail.Skip(1);
+
+            yield return (elem, head.Concat(tail));
+        }
+    }
+
     internal static IEnumerable<IEnumerable<TItem>> Permutations<TItem>(
         this IEnumerable<TItem> source)
+    {
+        if (source.Any())
         {
-            if (source.Any())
+            foreach (var item in source)
             {
-                foreach (var item in source)
+                var lists = source.Except(new List<TItem> {item})
+                                    .Permutations();
+                foreach (var list in lists)
                 {
-                    var lists = source.Except(new List<TItem> {item})
-                                      .Permutations();
-                    foreach (var list in lists)
-                    {
-                        yield return list.Prepend(item);
-                    }
+                    yield return list.Prepend(item);
                 }
             }
-            else
-            {
-                yield return Enumerable.Empty<TItem>();
-            }
         }
+        else
+        {
+            yield return Enumerable.Empty<TItem>();
+        }
+    }
 }
 
 public class EnumerableExtensionsTest
@@ -49,13 +71,37 @@ public class EnumerableExtensionsTest
     }
 
     [Fact]
+    public void Extract()
+    {
+        var array = new int[] { 1, 2, 3, 5, 5 };
+        Assert.Equal(array, array.Extract().Select(pair => pair.Item1));
+        Assert.Collection(array.Extract(),
+            p1 =>
+            {
+                // Why does this fail?
+                // Assert.Equal((1, new int [] {2, 3, 5, 5}), (p1.Item1, p1.Item2.ToArray()));
+                Assert.Equal(1, p1.Item1);
+                Assert.Equal([2, 3, 5, 5], p1.Item2.ToArray());
+            },
+            p2 => Assert.Equal([1, 3, 5, 5], p2.Item2.ToArray()),
+            p3 => Assert.Equal([1, 2, 5, 5], p3.Item2.ToArray()),
+            p4 => Assert.Equal([1, 2, 3, 5], p4.Item2.ToArray()),
+            p5 => Assert.Equal([1, 2, 3, 5], p5.Item2.ToArray())
+        );
+    }
+
+    [Fact]
     public void Permutations()
     {
         var array = new int[] { 1, 2, 3 };
         Assert.Equal(1 * 2 * 3, array.Permutations().Count());
-        Assert.All(array.Permutations(), p =>
-            {
-                Assert.Equal([1, 2, 3], p.Order());
-            });
+        Assert.Collection(array.Permutations(),
+            p1 => Assert.Equal([1, 2, 3], p1),
+            p2 => Assert.Equal([1, 3, 2], p2),
+            p3 => Assert.Equal([2, 1, 3], p3),
+            p4 => Assert.Equal([2, 3, 1], p4),
+            p5 => Assert.Equal([3, 1, 2], p5),
+            p6 => Assert.Equal([3, 2, 1], p6)
+        );
     }
 }
