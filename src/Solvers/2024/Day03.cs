0b2d028f52f0dd715ff4b54aa01ebabe873151ea
Dmitry Tyurnikov <DimsFromDergachy@outlook.com>
2024-12-03 09:31:30 +0400

    Solve the day (Part B)

--- src/Solvers/2024/Day03.cs
+++ src/Solvers/2024/Day03.cs
@@ -28,3 +28,40 @@ public class DecorrupterTest
     }
 }
 
+[Solver(2024, 03, Part.B)]
+class DecorrupterDo : Solver
+{
+    Regex regex = new Regex("(do\\(\\)|don't\\(\\)|mul\\((\\d{1,3}),(\\d{1,3})\\))");
+
+    internal override object Solve(string input) =>
+        regex.Matches(input)
+             .Select(match => match.Groups)
+             .Aggregate(new Context{}, (context, group) =>
+                (group[1].Value, context.Do) switch
+                {
+                    ("do()",    _) => context with { Do = true },
+                    ("don't()", _) => context with { Do = false },
+                    (_, false)     => context,
+                    (_, true)      => context with {
+                        Sum = context.Sum + int.Parse(group[2].Value) * int.Parse(group[3].Value)
+                    },
+                })
+             .Sum;
+
+    struct Context
+    {
+        public Context() {}
+        internal bool Do { get; set; } = true;
+        internal int Sum { get; set; } = 0;
+    }
+}
+
+public class DecorrupterDoTest
+{
+    [Theory]
+    [InlineData(48, "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")]
+    public void Example(int expected, string input)
+    {
+        Assert.Equal(expected, new DecorrupterDo().Solve(input));
+    }
+}
