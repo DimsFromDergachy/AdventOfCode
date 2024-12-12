c5ad336be828be3c11fba6c8f1bf97ed1298886d
Dmitry Tyurnikov <DimsFromDergachy@outlook.com>
2024-12-12 11:22:35 +0400

    Solve the day (Part B)

--- src/Solvers/2024/Day12.cs
+++ src/Solvers/2024/Day12.cs
@@ -1,6 +1,7 @@
 namespace Year2024.Day12;
 
 [Solver(2024, 12, Part.A)]
+[Solver(2024, 12, Part.B)]
 class Gardener : Solver
 {
     List<(int dx, int dy)> dirs = new List<(int dx, int dy)>
@@ -11,7 +12,7 @@ class Gardener : Solver
         var map = input.Lines()
                        .ToArray();
 
-        return Solve(map).Select(pair => pair.area * pair.perimeter).Sum();
+        return Solve(map).Select(pair => (long) ((long)pair.area) * ((long)pair.perimeter)).Sum();
     }
 
     IEnumerable<(int area, int perimeter)> Solve(char[,] map)
@@ -27,25 +28,58 @@ class Gardener : Solver
             var (area, perimeter) = (0, 0);
             var stack = new Stack<(int x, int y)>();
             stack.Push(start.Index);
+            HashSet<((int x, int y) point, (int dx, int dy) dir)> fences = new();
 
             while (stack.Any())
             {
-                var current = stack.Pop();
-                if (visited[current.x, current.y])
+                var curr = stack.Pop();
+                if (visited[curr.x, curr.y])
                     continue;
 
-                visited[current.x, current.y] = true;
+                visited[curr.x, curr.y] = true;
                 area++;
+                fences.Add(((curr.x + 0, curr.y + 0), (+1,  0)));
+                fences.Add(((curr.x + 1, curr.y + 0), ( 0, +1)));
+                fences.Add(((curr.x + 1, curr.y + 1), (-1,  0)));
+                fences.Add(((curr.x + 0, curr.y + 1), ( 0, -1)));
 
                 foreach (var dir in dirs)
                 {
                     try
                     {
-                        (int x, int y) next = (current.x + dir.dx, current.y + dir.dy);
+                        (int x, int y) next = (curr.x + dir.dx, curr.y + dir.dy);
 
                         if (map[next.x, next.y] == plant)
                         {
                             perimeter--; // Neighbor eats a perimeter
+
+                            switch (dir)
+                            {
+                                case (1, 0):
+                                {
+                                    fences.Remove(((curr.x + 1, curr.y + 0), ( 0, +1)));
+                                    fences.Remove(((next.x + 0, next.y + 1), ( 0, -1)));
+                                    break;
+                                }
+                                case (0, 1):
+                                {
+                                    fences.Remove(((curr.x + 1, curr.y + 1), (-1,  0)));
+                                    fences.Remove(((next.x + 0, next.y + 0), (+1,  0)));
+                                    break;
+                                }
+                                case (-1, 0):
+                                {
+                                    fences.Remove(((curr.x + 0, curr.y + 1), ( 0, -1)));
+                                    fences.Remove(((next.x + 1, next.y + 0), ( 0, +1)));
+                                    break;
+                                }
+                                case (0, -1):
+                                {
+                                    fences.Remove(((curr.x + 0, curr.y + 0), (+1,  0)));
+                                    fences.Remove(((next.x + 1, next.y + 1), (-1,  0)));
+                                    break;
+                                }
+                            };
 
                             if (visited[next.x, next.y])
                                 continue;
@@ -57,7 +91,59 @@ class Gardener : Solver
                 }
             }
 
-            yield return (area, 4 * area + perimeter);
+            var hs1 = fences.GroupBy(fence => fence.dir)
+                            .First(gr => gr.Key == (+1, 0))
+                            .Select(fence => fence.point)
+                            .GroupBy(fence => fence.y)
+                            .Select(gr => gr.OrderBy(fence => fence.x))
+                            .Select(gr => gr.Aggregate((-2, 0), (res, point) => {
+                                if (res.Item1 + 1 < point.x)
+                                    return (point.x, res.Item2 + 1);
+                                else
+                                    return (point.x, res.Item2);
+                                }))
+                           .Sum(gr => gr.Item2);
+
+            var hs2 = fences.GroupBy(fence => fence.dir)
+                            .First(gr => gr.Key == (-1, 0))
+                            .Select(fence => fence.point)
+                            .GroupBy(fence => fence.y)
+                            .Select(gr => gr.OrderBy(fence => fence.x))
+                            .Select(gr => gr.Aggregate((-2, 0), (res, point) => {
+                                if (res.Item1 + 1 < point.x)
+                                    return (point.x, res.Item2 + 1);
+                                else
+                                    return (point.x, res.Item2);
+                                }))
+                           .Sum(gr => gr.Item2);
+
+            var vs1 = fences.GroupBy(fence => fence.dir)
+                            .First(gr => gr.Key == (0, +1))
+                            .Select(fence => fence.point)
+                            .GroupBy(fence => fence.x)
+                            .Select(gr => gr.OrderBy(fence => fence.y))
+                            .Select(gr => gr.Aggregate((-2, 0), (res, point) => {
+                                if (res.Item1 + 1 < point.y)
+                                    return (point.y, res.Item2 + 1);
+                                else
+                                    return (point.y, res.Item2);
+                                }))
+                            .Sum(gr => gr.Item2);
+
+            var vs2 = fences.GroupBy(fence => fence.dir)
+                            .First(gr => gr.Key == (0, -1))
+                            .Select(fence => fence.point)
+                            .GroupBy(fence => fence.x)
+                            .Select(gr => gr.OrderBy(fence => fence.y))
+                            .Select(gr => gr.Aggregate((-2, 0), (res, point) => {
+                                if (res.Item1 + 1 < point.y)
+                                    return (point.y, res.Item2 + 1);
+                                else
+                                    return (point.y, res.Item2);
+                                }))
+                            .Sum(gr => gr.Item2);
+
+            yield return (area, hs1 + vs1 + hs2 + vs2);
         }
     }
 }
@@ -65,7 +151,7 @@ class Gardener : Solver
 public class GardenerTest
 {
     [Fact]
-    internal void Example()
+    internal void ExampleA()
     {
         var input = @"
 OOOOO
@@ -75,6 +161,41 @@ OXOXO
 OOOOO
 ";
 
-        Assert.Equal(772, new Gardener().Solve(input));
+        // Assert.Equal(772, new Gardener().Solve(input));
+        Assert.Equal(436L, new Gardener().Solve(input));
+    }
+
+    [Fact]
+    internal void ExampleB()
+    {
+        var input = @"
+AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA
+";
+
+        Assert.Equal(368L, new Gardener().Solve(input));
+    }
+
+    [Fact]
+    internal void ExampleC()
+    {
+        var input = @"
+RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE
+";
+
+        Assert.Equal(1206L, new Gardener().Solve(input));
     }
 }
