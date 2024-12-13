ec79a32075d7ed0aa4e374708552b64f826a0419
Dmitry Tyurnikov <DimsFromDergachy@outlook.com>
2024-12-13 10:07:08 +0400

    Solve the day (Part B)

--- src/Solvers/2024/Day13.cs
+++ src/Solvers/2024/Day13.cs
@@ -20,7 +20,7 @@ class Winner : Solver
                     .Sum(pair => 3 * pair.A + pair.B);
     }
 
-    int[] Parse(string[] line)
+    long[] Parse(string[] line)
     {
         var a = A.Match(line[0]).Groups;
         var b = B.Match(line[1]).Groups;
@@ -31,22 +31,71 @@ class Winner : Solver
                 b[1].Value, b[2].Value,
                 p[1].Value, p[2].Value,
             }
-            .Select(x => int.Parse(x))
+            .Select(x => long.Parse(x))
             .ToArray();
     }
 
-    IEnumerable<(int A, int B)> Solve(int[] puzzle) =>
-        Enumerable.Range(0, 101)
-                  .Select(A => (A, B: (puzzle[4] - A * puzzle[0]) / puzzle[2]))
-                  .Where(pair => pair.A * puzzle[0] + pair.B * puzzle[2] == puzzle[4]
-                                    &&
-                                 pair.A * puzzle[1] + pair.B * puzzle[3] == puzzle[5]);
+    IEnumerable<(long A, long B)> Solve(long[] pz)
+    {
+        pz[4] += 10000000000000;
+        pz[5] += 10000000000000;
+        long A, B;
+        if ((pz[0] * pz[3] - pz[1] * pz[2]) == 0)
+        {
+            if (pz[0] > 3 * pz[2])
+            {
+                A = pz[4] / pz[0];
+                B = pz[4] % pz[0] / pz[2];
+            }
+            else
+            {
+                A = pz[4] % pz[2] / pz[0];
+                B = pz[4] / pz[2];
+            }
+        }
+        else
+        {
+            A = (pz[4] * pz[3] - pz[5] * pz[2]) / (pz[0] * pz[3] - pz[1] * pz[2]);
+            B = (pz[4] * pz[1] - pz[5] * pz[0]) / (pz[1] * pz[2] - pz[0] * pz[3]);
+        }
+
+        if (A >= 0 && B >= 0
+            &&
+            A * pz[0] + B * pz[2] == pz[4]
+            &&
+            A * pz[1] + B * pz[3] == pz[5])
+            yield return (A, B);
+    }
+
 }
 
 public class WinnerTest
 {
     [Fact]
-    internal void Example()
+    internal void ExampleB()
+    {
+        var input = @"
+Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=10000000008400, Y=10000000005400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=10000000012748, Y=10000000012176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=10000000007870, Y=10000000006450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=10000000018641, Y=10000000010279
+";
+        //Assert.Equal(480L, new Winner(Part.B).Solve(input));
+    }
+
+    [Fact]
+    internal void ExampleA()
     {
         var input = @"
 Button A: X+94, Y+34
@@ -65,6 +114,40 @@ Button A: X+69, Y+23
 Button B: X+27, Y+71
 Prize: X=18641, Y=10279
 ";
-        Assert.Equal(480, new Winner(Part.A).Solve(input));
+        Assert.Equal(480L, new Winner(Part.A).Solve(input));
+    }
+
+    [Fact]
+    public void SpecialCase()
+    {
+        var input = @"
+Button A: X+1, Y+2
+Button B: X+2, Y+4
+Prize: X=0, Y=0";
+        Assert.Equal(0L, new Winner(Part.A).Solve(input));
+
+        input = @"
+Button A: X+1, Y+2
+Button B: X+2, Y+4
+Prize: X=1, Y=1";
+        Assert.Equal(0L, new Winner(Part.A).Solve(input));
+
+        input = @"
+Button A: X+1, Y+2
+Button B: X+2, Y+4
+Prize: X=1, Y=2";
+        Assert.Equal(3L, new Winner(Part.A).Solve(input));
+
+        input = @"
+Button A: X+1, Y+2
+Button B: X+2, Y+4
+Prize: X=2, Y=4";
+        Assert.Equal(1L, new Winner(Part.A).Solve(input));
+
+        input = @"
+Button A: X+1, Y+2
+Button B: X+2, Y+4
+Prize: X=15, Y=30";
+        Assert.Equal(3L + 7L, new Winner(Part.A).Solve(input));
     }
 }
