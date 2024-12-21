a43d1f288c28ffa05c7dcdfb7a9f4f5284fbd199
Dmitry Tyurnikov <DimsFromDergachy@outlook.com>
2024-12-22 01:17:35 +0400

    No solution for Part B

--- src/Solvers/2024/Day21.cs
+++ src/Solvers/2024/Day21.cs
@@ -1,26 +1,56 @@
 namespace Year2024.Day21;
 
 [Solver(2024, 21, Part.A)]
+[Solver(2024, 21, Part.B)]
 internal class RemoteController : Solver
 {
-    internal RemoteController(Part part) : base(part) {}
-
+    int ChainLength = 2;
+    internal RemoteController(Part part) : base(part)
+    {
+        #pragma warning disable CS8524
+        ChainLength = Part switch
+        {
+            Part.A => 2, Part.B => 25,
+        };
+    }
+    internal RemoteController(Part part, int chainLength) : this(part)
+    {
+        ChainLength = chainLength;
+    }
     internal override object Solve(string input)
     {
-        Remote[] robots =
-        [
-            new RemoteNumPad(),
-            new RemoteDirPad(),
-            new RemoteDirPad(),
-        ];
+        Remote[] robots = Enumerable.Range(0, ChainLength)
+                                    .Select(_ => new RemoteDirPad())
+                                    .OfType<Remote>()
+                                    .Prepend(new RemoteNumPad())
+                                    .ToArray();
+
+        // if (ChainLength >= 20)
+        // {
+        //     throw new NotImplementedException();
+        // }
+        // return input.Lines()
+        //             .Select(line => long.Parse(line.SkipLast(1).ToArray())
+        //                         *
+        //                     robots.Aggregate(line.Skip(0),
+        //                                     (l, r) => r.Moves(l)).Count())
+        //             .Sum();
 
         return input.Lines()
-                    .Select(line => int.Parse(line.SkipLast(1)
-                                                  .ToArray())
-                        * robots.Aggregate(line.Skip(0),
-                                          (line, robot) => line.SelectMany(ch => robot.Move(ch)))
-                                .Count())
-                    .Sum();
+                    .Sum(Solve2);
+    }
+
+    long Solve2(string input)
+    {
+        var line = new RemoteNumPad().Moves(input).ToArray();
+        var dyno = new MultiRemoteDirPad(ChainLength).GetDyno();
+
+        return long.Parse(input.SkipLast(1)
+                               .ToArray())
+                    *
+               line.Prepend('A')
+                   .Zip(line)
+                   .Sum(pair => dyno[(pair.First, pair.Second, ChainLength)]);
     }
 
     internal abstract class Remote
@@ -34,6 +64,8 @@ internal class RemoteController : Solver
             (pos, var prevs) = (moveTo, pos);
             return moves[this[prevs], this[moveTo]].Trim().Append('A');
         }
+
+        internal IEnumerable<char> Moves(IEnumerable<char> chs) => chs.SelectMany(Move);
 
         internal int this[char ch] => @this(ch);
         internal abstract int @this(char ch);
@@ -102,8 +134,9 @@ internal class RemoteController : Solver
         // | < | v | > |
         // +---+---+---+
 
-        internal RemoteDirPad()
+        internal RemoteDirPad(char pos = 'A')
         {
+            base.pos = pos;
             moves = new string[5, 5] {
 //-----++++-----++-----++-----++-----++-----++-----++
 //  #  ||||  ^  ||  A  ||  <  ||  v  ||  >  ||     ||
@@ -127,6 +160,49 @@ internal class RemoteController : Solver
             '>' => 4,
         };
     }
+
+    internal class MultiRemoteDirPad
+    {
+        int StackSize = 0;
+        public MultiRemoteDirPad(int stackSize) { StackSize = stackSize; }
+        Dictionary<(char a, char b, int level), long> dyno = new();
+
+        internal Dictionary<(char a, char b, int level), long> GetDyno()
+        {
+            for (int level = 0; level <= StackSize; level++)
+            {
+                foreach (char a in "^A<v>")
+                foreach (char b in "^A<v>")
+                {
+                    if (level == 0)
+                    {
+                        dyno.Add((a, b, level), 1);
+                        continue;
+                    }
+
+                    if (level == 1)
+                    {
+                        var robot1 = new RemoteDirPad(a);
+                        var moves1 = robot1.Moves([b]).ToArray();
+
+                        dyno.Add((a, b, level), moves1.Count());
+                        continue;
+                    }
+
+                    var robot = new RemoteDirPad(a);
+                    var moves = robot.Moves([b]).Prepend('A').ToArray();
+                    var pairs = moves.Zip(moves.Skip(1));
+
+                    dyno.Add((a, b, level), pairs.Select(pair =>
+                        dyno[(pair.First, pair.Second, level - 1)]).Sum());
+
+                }
+            }
+
+            return dyno;
+        }
+    }
+    // <|A  >>^A
 }
 
 public class RemoteControllerTest
@@ -137,7 +213,7 @@ public class RemoteControllerTest
     [InlineData(68 * 179, "179A")]
     [InlineData(64 * 456, "456A")]
     [InlineData(64 * 379, "379A")]
-    internal void Example(int complexity, string input)
+    internal void Example(long complexity, string input)
     {
         Assert.Equal(complexity, new RemoteController(Part.A).Solve(input));
     }
@@ -173,7 +249,6 @@ public class RemoteControllerTest
     [InlineData("^^<<A", "<AAv<AA>>^A")] // 37
     [InlineData("v<<AA>^AA>A", "v<A<AA>>^AAvA^<A>AAvA^A")] // 37
     [InlineData("<AAv<AA>>^A", "v<<A>>^AAv<A<A>>^AAvAA^<A>A")] // 37
-
     internal void RemoteDirPad(string input, string expected)
     {
         var rnp = new RemoteController.RemoteDirPad();
@@ -261,5 +336,81 @@ public class RemoteControllerTest
                 }
             }
         }
+    }
+
+    [Theory]
+    [InlineData(  "7A",  0,         12L *  7L)]
+    [InlineData("029A",  0,         12L * 29L)]
+    [InlineData("029A",  1,         28L * 29L)]
+    [InlineData("029A",  2,         68L * 29L)]
+    [InlineData("029A",  3,        172L * 29L)]
+    [InlineData("029A",  4,        434L * 29L)]
+    [InlineData("029A",  5,       1116L * 29L)]
+    [InlineData("029A", 15,   13906986L * 29L)]
+    // [InlineData("029A", 20, 1553478224L * 29L)]
+
+    [InlineData("980A", 15,   11880624280L)]
+    [InlineData("179A", 15,    2428011848L)]
+    [InlineData("456A", 15,    6185325360L)]
+    [InlineData("379A", 15,    4942980914L)]
+
+    [InlineData("965A", 15,    13127250120)]
+    [InlineData("143A", 15,     2151376656)]
+    [InlineData("528A", 15,     7667852544)]
+    [InlineData("670A", 15,     9327865960)]
+    [InlineData("973A", 15,    13236079010)]
+
+    [InlineData("965A", 25,    163801595854770)]
+    [InlineData("143A", 25,     26844844069758)]
+    [InlineData("528A", 25,     95679342715968)]
+    [InlineData("670A", 25,     116392998233620)]
+    [InlineData("973A", 25,    165159536547740)]
+
+    internal void PartB(string input, int chain, long expected)
+    {
+        Assert.Equal(expected, (long) new RemoteController(Part.B, chain).Solve(input));
+        // Assert.Equal(        28L, (long) new RemoteController(Part.B,  1).Solve(input) / 29L);
+        // Assert.Equal(        68L, (long) new RemoteController(Part.B,  2).Solve(input) / 29L);
+        // Assert.Equal(       172L, (long) new RemoteController(Part.B,  3).Solve(input) / 29L);
+        // Assert.Equal(       434L, (long) new RemoteController(Part.B,  4).Solve(input) / 29L);
+        // Assert.Equal(      1116L, (long) new RemoteController(Part.B,  5).Solve(input) / 29L);
+        // Assert.Equal(  13906986L, (long) new RemoteController(Part.B, 15).Solve(input) / 29L);
+        // Assert.Equal(1553478224L, (long) new RemoteController(Part.B, 20).Solve(input) / 29L);
+    }
+
+    [Theory]
+    [InlineData("^",  0,      1)] // ^
+    // [InlineData("^",  1,      2)] // <A
+    // [InlineData("^",  2,      8)] // v<<A>>^A
+    // [InlineData("^",  3,     18)] // v<A<AA>>^AvAA<^A>A
+    // [InlineData("^",  4,     46)] // 
+    // [InlineData("^",  5,    116)] // 
+    // [InlineData("^",  6,    298)] // 
+    // [InlineData("^",  7,    762)] // 
+    // [InlineData("^",  8,   1958)] // 
+    // [InlineData("^",  9,   5024)] // 
+    // [InlineData("^", 10,  12904)] // 
+    // [InlineData("^", 11,  33134)] // 
+    // [InlineData("^", 12,  85094)] // 
+    // [InlineData("^", 13, 218526)] // 
+    // [InlineData("^", 25, 17983595930)] // 
+    internal void PatternOfPartB(string ch, int rs, int expected)
+    {
+        // var robots = Enumerable.Range(0, rs)
+        //                        .Select(_ => new RemoteController.RemoteDirPad())
+        //                        .ToArray();
+
+        // var result = robots.Aggregate(ch.Skip(0), (s, r) => r.Moves(s));
+        // Assert.Equal(expected, result.LongCount());
+
+        var dyno = new RemoteController.MultiRemoteDirPad(25).GetDyno();
+        Assert.Equal(          2L, dyno[('A', '^',  1)]);
+        Assert.Equal(          8L, dyno[('A', '^',  2)]);
+        Assert.Equal(      12904L, dyno[('A', '^', 10)]);
+        Assert.Equal(17983595930L, dyno[('A', '^', 25)]);
+
+        // 567878317421856 - too high
+        // 567878317421856
+        // 567878317421856
     }
 }
