60827e1e74d361eb2c4baa0fd7f01e9d4b80c714
Dmitry Tyurnikov <DimsFromDergachy@outlook.com>
2024-12-31 03:04:04 +0400

    Solve 22-th (Part B)

--- src/Solvers/2024/Day22.cs
+++ src/Solvers/2024/Day22.cs
@@ -3,40 +3,100 @@ using System.Collections;
 namespace Year2024.Day22;
 
 [Solver(2024, 22, Part.A)]
-class PseudoRandomGenerator : Solver
+[Solver(2024, 22, Part.B)]
+class BananaMarket : Solver
 {
+    public BananaMarket(Part part) : base(part) {}
+
     internal override object Solve(string input)
     {
-        return input.Parse<long>('\r', '\n')
-                    .Sum(s => Enumerable.Range(0, 2000)
-                                        .Aggregate(
-                                            new Generator(s),
-                                            (gen, _) => {
-                                                gen.MoveNext(); return gen;
-                                            })
-                                        .Current);
+        if (Part == Part.A)
+            return input.Lines()
+                        .Parse<long>()
+                        .Sum(s => new Generator(s).ToEnumerable().Last());
+
+        var gens = input.Lines()
+                        .Parse<long>()
+                        .Select(s => new Generator(s))
+                        .Select(gen => new GeneratorChanges(gen))
+                        .Zip(Enumerable.Range(0, int.MaxValue))
+                        .ToList();
+
+        var memo = new Dictionary<(int n, (int a, int b, int c, int d)), int>();
+
+        foreach (var (gen, x) in gens)
+            foreach (var seq in gen.ToEnumerable())
+                if (!memo.ContainsKey((x, seq)))
+                    memo.Add((x, seq), gen.Sale);
+
+        int maxSale = -1;
+
+        for (int a = -9; a <= 9; a++)
+        for (int b = -9; b <= 9; b++)
+        for (int c = -9; c <= 9; c++)
+        for (int d = -9; d <= 9; d++)
+        {
+            var sale = 0;
+            for (int x = 0; x < gens.Count(); x++)
+                if (memo.ContainsKey((x, (a, b, c, d))))
+                    sale += memo[(x, (a, b, c, d))];
+            maxSale = Math.Max(maxSale, sale);
+        }
+
+        return maxSale;
     }
 
     internal class Generator : IEnumerator<long>
     {
         const long PRUNE = 16777216;
-        long _secret;
+        long secret;
+        int rest;
 
-        public Generator(long seed)
+        public Generator(long seed, int rest = 2000)
         {
-            _secret = seed;
+            this.rest = rest;
+            secret = seed;
         }
 
-        public long Current => _secret;
+        public long Current => secret;
         object IEnumerator.Current => Current;
 
         public bool MoveNext()
         {
-            long s0 = _secret;
+            long s0 = secret;
             long s1 = ((s0 <<  6) ^ s0) % PRUNE;
             long s2 = ((s1 >>  5) ^ s1) % PRUNE;
-            _secret = ((s2 << 11) ^ s2) % PRUNE;
-            return true;
+            secret  = ((s2 << 11) ^ s2) % PRUNE;
+            return rest-- > 0;
+        }
+
+        public void Dispose() {}
+        public void Reset() {}
+    }
+
+    internal class GeneratorChanges : IEnumerator<(int a, int b, int c, int d)>
+    {
+        Generator gen;
+        public GeneratorChanges(Generator gen)
+        {
+            this.gen = gen;
+            a = (int) (gen.Current % 10);           gen.MoveNext();
+            b = (int) (gen.Current % 10);           gen.MoveNext();
+            c = (int) (gen.Current % 10);           gen.MoveNext();
+            d = (int) (gen.Current % 10);           gen.MoveNext();
+            e = (int) (gen.Current % 10);           gen.MoveNext();
+        }
+
+        internal int Sale => e;
+
+        int a, b, c, d, e;
+        public (int, int, int, int) Current => (b - a, c - b, d - c, e - d);
+        object IEnumerator.Current => Current;
+
+        public bool MoveNext()
+        {
+            (a, b, c, d, e) = (b, c, d, e, (int) (gen.Current % 10));
+            return gen.MoveNext();
         }
 
         public void Dispose() {}
@@ -44,22 +104,53 @@ class PseudoRandomGenerator : Solver
     }
 }
 
-public class PseudoRandomGeneratorTest
+public class BananaMarketTest
 {
     [Fact]
-    internal void Generator123()
+    internal void Generators()
     {
-        var gen = new PseudoRandomGenerator.Generator(123);     _ = gen.MoveNext();
+        var gen1 = new BananaMarket.Generator(123);
+        var gen2 = new BananaMarket.GeneratorChanges(gen1);
+        var list = gen2.ToEnumerable().ToList();
 
-        Assert.Equal(15887950, gen.Current);                    _ = gen.MoveNext();
-        Assert.Equal(16495136, gen.Current);                    _ = gen.MoveNext();
-        Assert.Equal(  527345, gen.Current);                    _ = gen.MoveNext();
-        Assert.Equal(  704524, gen.Current);                    _ = gen.MoveNext();
-        Assert.Equal( 1553684, gen.Current);                    _ = gen.MoveNext();
-        Assert.Equal(12683156, gen.Current);                    _ = gen.MoveNext();
-        Assert.Equal(11100544, gen.Current);                    _ = gen.MoveNext();
-        Assert.Equal(12249484, gen.Current);                    _ = gen.MoveNext();
-        Assert.Equal( 7753432, gen.Current);                    _ = gen.MoveNext();
-        Assert.Equal( 5908254, gen.Current);                    _ = gen.MoveNext();
+        Assert.Equal((-3, 6, -1, -1), list.First());
+        Assert.Equal(1996, list.Count());
+    }
+
+    [Fact]
+    internal void ExampleA()
+    {
+        var input = @"
+1
+10
+100
+2024
+";
+
+        Assert.Equal(37327623L, new BananaMarket(Part.A).Solve(input));
+    }
+
+    [Fact]
+    internal void PartA()
+    {
+        var input = File.ReadAllText("./Solvers/2024/Data/Day22");
+
+        Assert.Equal(17724064040L, new BananaMarket(Part.A).Solve(input));
+
+        // take about 30 seconds
+        // Assert.Equal(1998, new BananaMarket(Part.B).Solve(input));
+    }
+
+    [Fact]
+    internal void ExampleB()
+    {
+        var input = @"
+1
+2
+3
+2024
+";
+
+        Assert.Equal(23, new BananaMarket(Part.B).Solve(input));
     }
 }
