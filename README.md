4be02eed683b87f2d44d7b312d9c75982bd49485
Dmitry Tyurnikov <DimsFromDergachy@outlook.com>
2025-12-02 14:23:35 +0400

    Solve 1-st day

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 > dotnet run --project src/src.csproj 2024 14 B
 
+* End of input -- ```Control + D``` twice
+
 #### Day 14: Part two
 
 ![Should be a picture of a Christmas tree](./src/Solvers/2024/Data/Day14_008159.jpg "The Easter egg")
--- src/Solvers/2025/Day01.cs
+++ src/Solvers/2025/Day01.cs
@@ -1,13 +1,69 @@
 namespace Year2025.Day01;
 
 [Solver(2025, 01, Part.A)]
-class Day01 : Solver
+[Solver(2025, 01, Part.B)]
+class RotaryLock : Solver
 {
-    internal Day01(Part part) : base(part) { }
+    internal RotaryLock(Part part) : base(part) { }
 
     internal override object Solve(string input)
     {
-        throw new NotImplementedException("Coming soon ...");
+        var inputs = input.Replace('L', '-')
+                          .Replace('R', '+')
+                          .Words()
+                          .Parse<int>();
+
+        var rotary = new Rotary();
+
+        #pragma warning disable CS8524
+        return Part switch
+        {
+            Part.A => inputs.Scan(50, MoveA)
+                            .Count(x => x % 100 == 0),
+            Part.B => inputs.Aggregate(new Rotary(), (r, x) => r.Rotate(x))
+                            .Clicks,
+        };
+    }
+
+    int MoveA(int acc, int rotate) => acc + rotate;
+
+    class Rotary
+    {
+        internal int Position { get; private set; } = 50;
+        internal int Clicks { get; private set; }
+
+        internal Rotary Rotate(int rotate)
+        {
+            while (rotate != 0)
+                rotate = RotateToClick(rotate);
+            return this;
+        }
+
+        private int RotateToClick(int rotate)
+        {
+            if (Position == 0 && rotate < 0)
+                Position = 100;
+
+            if (Position + rotate >= 100)
+            {
+                (Position, rotate) = (Position + (100 - Position), rotate - (100 - Position));
+                Position = 0;
+                Clicks++;
+                return rotate;
+            }
+
+            if (Position + rotate <= 0)
+            {
+                (Position, rotate) = (Position - Position, rotate + Position);
+                Clicks++;
+                return rotate;
+            }
+
+            Position += rotate;
+            Assert.True(Position >   0);
+            Assert.True(Position < 100);
+            return 0;
+        }
     }
 }
 
@@ -18,13 +74,41 @@ public class Test
     public void Example()
     {
         var input = @"
-some input 1
-some input 2
-";
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82";
+        Assert.Equal(3, new RotaryLock(Part.A).Solve(input));
 
-        Assert.Fail("DO NOT TEST ME (!!!)");
+        Assert.Equal(1, new RotaryLock(Part.B).Solve(string.Join(" ",
+            input.Lines().Take(1))));
+        Assert.Equal(1, new RotaryLock(Part.B).Solve(string.Join(" ",
+            input.Lines().Take(2))));
+        Assert.Equal(2, new RotaryLock(Part.B).Solve(string.Join(" ",
+            input.Lines().Take(3))));
+        Assert.Equal(3, new RotaryLock(Part.B).Solve(string.Join(" ",
+            input.Lines().Take(5))));
+        Assert.Equal(4, new RotaryLock(Part.B).Solve(string.Join(" ",
+            input.Lines().Take(6))));
 
-        Assert.Equal(11, new Day01(Part.A).Solve(input));
-        Assert.Equal(31, new Day01(Part.B).Solve(input));
+        Assert.Equal(6, new RotaryLock(Part.B).Solve(input));
+    }
+
+    [Theory]
+    [InlineData("R50", 1)]
+    [InlineData("L50", 1)]
+    [InlineData("R1", 0)]
+    [InlineData("L1", 0)]
+    [InlineData("R99", 0)]
+    [InlineData("L99", 0)]
+    public void OneStep(string input, int expected)
+    {
+        Assert.Equal(expected, new RotaryLock(Part.A).Solve(input));
     }
 }
